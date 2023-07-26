3ed5c799188fd00cf4a80427d54fd1da9f63057c
sachintha-chathuranga <mmschathuranga1@gmail.com>
2023-07-26 19:38:05 +0530

    fix emoji erro

--- src/component/Outgoing.jsx
+++ src/component/Outgoing.jsx
@@ -1,10 +1,16 @@
-import { memo } from "react";
+import { memo, useRef } from "react";
 
 const Outgoing = ({msg}) => {
+    const msgs = useRef(msg);
+    if(msgs.current.match(/^[\da-fA-F]{4,5}$/)){
+        let emoji = msgs.current.padStart(5,'O');
+        emoji = String.fromCharCode(parseInt(emoji, 16));
+        msgs.current = emoji;
+    }
     return (
         <div className="chat outgoing">
             <div className="details">
-                <p>{msg}</p>
+                <p>{msgs.current}</p>
             </div>
         </div>
     )
